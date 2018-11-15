65c5ced7d1278166978b54b915dcd52510d941c7
cheekykite <xxue123@gmail.com>
2018-11-15 15:28:36 +0900

    여러 node 에 racadm 명령을 옵션 형식으로 보낼수 있는 예제.

---
+++ racadm_node_all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Copyright (c) 2018 by Dasandata Corp.
+# run as a root
+
+ROOT_UID=0
+
+if [ "$UID" != "$ROOT_UID" ]; then
+  echo "Must be a root to run this script."
+  exit 1
+fi
+
+
+if [ -z $1 ]; then
+  echo "Usage: bash racadm_node_all.sh getniccfg"
+  exit 1
+fi
+
+
+for NUM in 1 2 3
+do echo "NODE $NUM"
+racadm --nocertwarn -r node$NUM.idrac -u root -p 123456qwer! $1
+done
+
+echo "End"
