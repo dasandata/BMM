2b3fe41cfd01785fe26ae0f2155393fd5a0ad35f
cheekykite <xxue123@gmail.com>
2020-04-24 11:06:36 +0900

    * 추가.

--- InfiniBand-limits-conf.sh
+++ InfiniBand-limits-conf.sh
@@ -1,11 +1,11 @@
 
 # /etc/security/limits.conf
 
-hard memlock unlimited
-soft memlock unlimited
+* hard memlock unlimited
+* soft memlock unlimited
 
-hard nofile 65535
-soft nofile 65535
+* hard nofile 65535
+* soft nofile 65535
 
-hard stack unlimited
-soft stack unlimited
+* hard stack unlimited
+* soft stack unlimited
